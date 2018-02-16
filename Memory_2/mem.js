c532f829d318522e14f4383b9d1e296a6e760182
DarkVasDehors <sibade.arnaud@gmail.com>
2018-02-16 17:22:41 +0100

    AngularJS avec le json

--- Memory_2/mem.js
+++ Memory_2/mem.js
@@ -1,7 +1,23 @@
-window.onload = function() {
-  console.log("test");
-  document.getElementByClassName("col-sm-4 thumbnail").addEventListener("click", function(){
-    let id = this.id;
-    console.log(id);
-  }
-}
+angular.module('memo', []).controller('leMemo',["$scope", "$http", function ($scope, $http){
+/*  var mesCartes = this;
+  mesCartes.cartes = [
+    {text:'Taric le male alpha', img:'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Taric_0.jpg'},
+    {text:'Taric le plus beau', img:'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Taric_2.jpg'},
+    {text:'Taric best champ', img:'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Taric_4.jpg'}
+  ]; */
+
+
+  $http.get('./taric.json').then(function(taricJson) {
+        $scope.mesTarics = taricJson.data.data;
+    });
+
+    function getRandomTaric(){
+      let TaricRandom = $scope.mesTarics[Object.keys($scope.mesTarics)[getRandomNumber()]];
+      
+    };
+
+    function getRandomNumber(){
+        return Math.floor((Math.random()*($scope.longueurMesTarics-1))+1);
+    }
+
+}]);
