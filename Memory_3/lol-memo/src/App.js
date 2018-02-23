54ef4e9e08c2ff38ef98e8d6b121270497dde383
DarkVasDehors <sibade.arnaud@gmail.com>
2018-02-23 17:52:05 +0100

    Tentative d'importation de json

--- Memory_3/lol-memo/src/App.js
+++ Memory_3/lol-memo/src/App.js
@@ -1,18 +1,72 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
+
+classAppextendsComponent{state={taric:[]}
+
+componentDidMount(){
+  fetch('/').then(res=>taric.json()).then(taric=>this.setState({taric}));
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div class="tout">
+         <div class="container">
+           <div class="navbar-header">
+             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+               <span class="sr-only">Toggle navigation</span>
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+             </button>
+             <a class="navbar-brand" href="#">Taric le plus beau #NoHomo</a>
+             </div>
+             <div id="navbar" class="collapse navbar-collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="container">
+            <div class="row" >
+
+
+            {this.state.users.map(user => <div key={taric.id}>{taric.image}</div>)}
+
+
+            <article class="col-sm-4 thumbnail" id="ivern1">
+              <img src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ivern_0.jpg"/>
+              <p class="textMem">Ivern le hippy</p>
+            </article>
+            <article class="col-sm-4 thumbnail" id="taric1">
+              <img src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Taric_0.jpg"/>
+              <p class="textMem">Taric le plus beau</p>
+            </article>
+            <article class="col-sm-4 thumbnail" id="garen1">
+              <img src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Garen_0.jpg"/>
+              <p class="textMem">Garen best champ</p>
+            </article>
+
+            <article class="col-sm-4 thumbnail" id="ivern2">
+              <img src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ivern_1.jpg"/>
+              <p class="textMem">Ivern le hippy</p>
+            </article>
+            <article class="col-sm-4 thumbnail" id="taric2">
+              <img src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Taric_2.jpg"/>
+              <p class="textMem">Taric le plus beau</p>
+            </article>
+            <article class="col-sm-4 thumbnail" id="garen2">
+              <img  src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Garen_4.jpg"/>
+              <p class="textMem">Garen best champ</p>
+            </article>
+
+
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> lol memo.
-        </p>
       </div>
     );
   }
